3a76cb0aa3abb001729b527dbf699cf365843612
Trần Công Mạnh <120402571+Congmanh270504@users.noreply.github.com>
2024-06-30 22:48:20 +0700

    #1- chiêm nghiệm (how to  use useEffect)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component, useEffect, useState, useRef } from 'react';
 import './App.css';
 import User from './component/User';
 import ListUser from './component/ListUser';
@@ -21,13 +21,26 @@ function App() {
     age: 30,
   }]);
   const addUser = (obj) => {
-    updateData([obj, ...userInfo]) // add user 
+    updateData([obj, ...userInfo]); // add user 
   }
   const deleteUser = (id) => { // lấy cái id của ng dùng click vô 
     let tmp = userInfo
     tmp = tmp.filter(user => user.id !== id) // tạo biến tmp để lưu lại giá trị userInfo sau đó filter, n~ cái user.id nào khác id với id user thì giữ ko thì vứt 
     updateData(tmp)
   }
+  // useEffect <=> componentdidmount chạy sau mỗi lần render(load trang)
+  useEffect(() => {
+    setTimeout(() => {
+      document.title = "U did use the CDM ";
+    }, 2000);
+  });
+  useEffect(() => {
+    // Mã này sẽ chạy mỗi khi userInfo thay đổi
+    console.log(userInfo);
+    if (userInfo.length === 5) {
+      alert('me')
+    }
+  }, [userInfo]); // Thêm userInfo vào mảng phụ thuộc
   return (
     <>
       <User addUser={addUser} />
